4d7e639e4f9349a76989c4b2a90e603791890ea3
Unknown <aleks.alemasov@mail.ru>
2017-11-14 21:36:20 +0300

    Повторное добавление проекта

---
+++ Kollorii/Kolorii/Kolorii/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kolorii
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox2.Enabled = false;
+            if (comboBox1.Text=="грибы")
+            {
+                comboBox2.Enabled = true;
+                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
